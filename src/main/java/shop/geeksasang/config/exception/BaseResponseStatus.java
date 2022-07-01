e991d6fcc4d9ace721ddedba6c1267b2f841a810
happysubin <asb0711@gmail.com>
2022-07-01 22:18:41 +0900

    Feat: 멤버 생성 시 검증 로직 추가

--- src/main/java/shop/geeksasang/config/exception/BaseResponseStatus.java
+++ src/main/java/shop/geeksasang/config/exception/BaseResponseStatus.java
@@ -21,6 +21,9 @@ public enum BaseResponseStatus {
     EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
+    DIFFRENT_PASSWORDS(false,2004,"입력하신 두 비밀번호가 다릅니다."),
+    DUPLICATE_USER_LOGIN_ID(false, 2005, "중복되는 유저 아이디입니다"),
+    DUPLICATE_USER_EMAIL(false, 2006, "중복되는 유저 이메일입니다"),
 
 
     /**
--- src/main/java/shop/geeksasang/service/MemberService.java
+++ src/main/java/shop/geeksasang/service/MemberService.java
@@ -1,13 +1,18 @@
 package shop.geeksasang.service;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import shop.geeksasang.config.exception.BaseException;
 import shop.geeksasang.domain.Member;
 import shop.geeksasang.domain.University;
 import shop.geeksasang.dto.CreateMemberReq;
 import shop.geeksasang.repository.MemberRepository;
 import shop.geeksasang.repository.UniversityRepository;
+
+import static shop.geeksasang.config.exception.BaseResponseStatus.*;
 
 @Transactional
 @Service
@@ -21,18 +26,21 @@ public class MemberService {
     public Member createMember(CreateMemberReq dto){
 
          if(!dto.getCheckPassword().equals(dto.getPassword())) {
-             throw new RuntimeException();
+             throw new BaseException(DIFFRENT_PASSWORDS);
          }
 
-        //ToDo 로그인 아이디 중복
+        if(!memberRepository.findMemberByLoginId(dto.getLoginId()).isEmpty()){
+            throw new BaseException(DUPLICATE_USER_LOGIN_ID);
+        }
 
-
-        //ToDo email 중복
-
+        if(!memberRepository.findMemberByEmail(dto.getEmail()).isEmpty()){
+            throw new BaseException(DUPLICATE_USER_EMAIL);
+        }
 
         Member member = dto.toEntity();
         University university = universityRepository
-                .findUniversitiesByName(dto.getUniversityName()).orElseThrow(() -> new RuntimeException(""));
+                .findUniversitiesByName(dto.getUniversityName())
+                .orElseThrow(() -> new RuntimeException(""));
 
         member.connectUniversity(university);
         member.changeStatusToActive();
