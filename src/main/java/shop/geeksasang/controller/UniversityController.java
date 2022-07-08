eaf62e992a3cdc047025dab973393c7eb1c081e9
Juyeon0526 <anm0912@naver.com>
2022-07-08 17:35:29 +0900

    Fix: 대학교 목록 조회 swagger 추가

--- src/main/java/shop/geeksasang/controller/UniversityController.java
+++ src/main/java/shop/geeksasang/controller/UniversityController.java
@@ -1,5 +1,6 @@
 package shop.geeksasang.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class UniversityController {
     private final UniversityService universityService;
 
     // 대학교 조회: 전체 목록
+    @ApiOperation(value = "조회: 대학교 전체 목록 조회", notes = "회원가입 시 대학교 선택에서 대학교 전체 목록을 조회한다.")
     @GetMapping
     public BaseResponse<List<University>> getAllUniversity(){
         List<University> getUniversityRes = universityService.getAllUniversity();
