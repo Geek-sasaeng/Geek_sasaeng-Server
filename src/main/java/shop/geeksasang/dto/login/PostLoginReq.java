60143c8cc3f052b8e229d86b3ae4f810565b75e8
KimMinheee <99alsgmlalsl@naver.com>
2022-07-10 20:15:14 +0900

    Fix: request,response dto에 example 추가

--- src/main/java/shop/geeksasang/dto/login/PostLoginReq.java
+++ src/main/java/shop/geeksasang/dto/login/PostLoginReq.java
@@ -10,12 +10,12 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @Data
 public class PostLoginReq {
-    @ApiModelProperty(value="로그인 아이디, 최소 6자 이상")
+    @ApiModelProperty(value="로그인 아이디, 최소 6자 이상",example = "xhaktmchl15")
     @ApiParam(value = "사용자 ID", required = true)
     @Size(min = 6)
     private String loginId;
 
-    @ApiModelProperty(value="비밀번호, 최소 8자 이상")
+    @ApiModelProperty(value="비밀번호, 최소 8자 이상",example = "chlxorb!2")
     @ApiParam(value = "사용자 비밀번호", required = true)
     @Size(min = 8)
     private String password;
