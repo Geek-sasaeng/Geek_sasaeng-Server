4c2feeb67749987f59f55bfd4f5221f685c65732
xhaktmchl <xhaktmchl@naver.com>
2022-07-10 10:51:19 +0900

    fix: PatchPhoneNumber, PatchPhoneValidKey, CheckNickName,PatchInformation, PatchProfileImgUrl 관련Res DTO 에 swagger example추가

--- src/main/java/shop/geeksasang/dto/member/GetCheckNickNameDuplicatedRes.java
+++ src/main/java/shop/geeksasang/dto/member/GetCheckNickNameDuplicatedRes.java
@@ -1,10 +1,14 @@
 package shop.geeksasang.dto.member;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder // .builder() 사용
 @Data // Getter, Setter 포함
 public class GetCheckNickNameDuplicatedRes {
+    @ApiModelProperty(example = "긱사생")
+    @ApiParam(value = "중복검사 할 닉네임.")
     private String nickName;
 }
