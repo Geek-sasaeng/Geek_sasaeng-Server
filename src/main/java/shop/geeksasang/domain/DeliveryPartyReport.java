d6d0b6a6a2d1337345fc48c96936e06f5fa63902
happysubin <asb0711@gmail.com>
2022-07-21 14:07:14 +0900

    Feat: 신고 엔티티 개발

--- src/main/java/shop/geeksasang/domain/DeliveryPartyReport.java
+++ src/main/java/shop/geeksasang/domain/DeliveryPartyReport.java
@@ -1,13 +1,20 @@
 package shop.geeksasang.domain;
 
+
 import javax.persistence.*;
 
+
 @Entity
-@DiscriminatorValue("delivery_party")
-public class DeliveryPartyReport extends Report{
+@DiscriminatorValue("delivery_party_report")
+public class DeliveryPartyReport extends Report {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="delivery_party_id")
-    public DeliveryParty deliveryParty;
-
+    @ManyToOne
+    @JoinColumn(name = "delivery_party_id")
+    private DeliveryParty deliveryParty;
 }
+
+/**
+ * 게시물 신고
+ * 게시글 신고가 3개 이상일 때는, 그 게시글 삭제
+ * 이미 신고를 한 게시물에는 다시 신고를 할 수 없음
+ */
--- src/main/java/shop/geeksasang/domain/MemberReport.java
+++ src/main/java/shop/geeksasang/domain/MemberReport.java
@@ -1,9 +1,20 @@
 package shop.geeksasang.domain;
 
+
+import shop.geeksasang.config.status.ReportStatus;
+
 import javax.persistence.*;
 
 @Entity
-@DiscriminatorValue("member")
-public class MemberReport extends Report{
+@DiscriminatorValue("member_report")
+public  class MemberReport extends Report {
 
 }
+/**
+ * 사용자 신고
+ * 사용자 신고가 1달 3개 이상일때는, 사용자가 게시물을 못올림
+ * 1달마다 사용자 신고 카운트가 초기화가 됨
+ * 사용자 차단하면 신고자에게 그 사용자가 쓴 게시물이 안보임
+ * 신고자가 같은 사용자를 여러번 신고할 수 없음
+ */
+
