bd3122f558c65c7a7c16a09e655d35b344759398
happysubin <asb0711@gmail.com>
2022-07-24 03:43:31 +0900

    Feat: Swagger 명세 추가

--- src/main/java/shop/geeksasang/dto/deliveryParty/get/GetDeliveryPartiesRes.java
+++ src/main/java/shop/geeksasang/dto/deliveryParty/get/GetDeliveryPartiesRes.java
@@ -1,5 +1,7 @@
 package shop.geeksasang.dto.deliveryParty.get;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Getter;
 import lombok.Setter;
 import shop.geeksasang.dto.deliveryParty.get.vo.DeliveryPartiesVo;
@@ -9,7 +11,11 @@ import java.util.List;
 @Getter @Setter
 public class GetDeliveryPartiesRes {
 
+    @ApiModelProperty(example = "true")
+    @ApiParam(value = "마지막 페이지면 true, 마지막 페이지가 아니면 false")
     private boolean isFinalPage;
+
+    @ApiParam(value = "가져온 배달 파티 리스트")
     private List<DeliveryPartiesVo> deliveryPartiesVoList;
 
     public GetDeliveryPartiesRes(boolean isFinalPage, List<DeliveryPartiesVo> deliveryPartiesVoList) {
