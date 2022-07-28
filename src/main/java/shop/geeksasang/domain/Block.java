1162eb179706e2cba04504cd6ba5bb6562481459
happysubin <asb0711@gmail.com>
2022-07-28 23:09:21 +0900

    Feat: 차단 도메인 구현

---
+++ src/main/java/shop/geeksasang/domain/Block.java
@@ -0,0 +1,33 @@
+package shop.geeksasang.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import shop.geeksasang.config.domain.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Block extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "block_id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocking_member_id")
+    private Member blockingMember;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocked_member_id")
+    private Member blockedMember;
+
+    @Builder
+    public Block(Member blockingMember, Member blockedMember) {
+        this.blockingMember = blockingMember;
+        this.blockedMember = blockedMember;
+    }
+}
