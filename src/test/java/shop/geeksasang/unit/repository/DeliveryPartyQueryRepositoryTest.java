70f6092a64b9d06cefc007255bef4fa5c9396c3f
happysubin <asb0711@gmail.com>
2022-08-08 14:23:58 +0900

    Feat: 통합 테스트 추상 클래스 작성

--- src/test/java/shop/geeksasang/unit/repository/DeliveryPartyQueryRepositoryTest.java
+++ src/test/java/shop/geeksasang/unit/repository/DeliveryPartyQueryRepositoryTest.java
@@ -1,5 +1,6 @@
 package shop.geeksasang.unit.repository;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
