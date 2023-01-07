4f9ce9282e5ecf8082d1f889d5a6ec68c7b4c231
xhaktmchl <xhaktmchl@naver.com>
2023-01-07 18:18:04 +0900

    #237 fix: 채팅 읽음 요청API-Chat 엔티티 @NoArgsConstructor추가로 트러블 슈팅

--- src/main/java/shop/geeksasang/domain/chat/Chat.java
+++ src/main/java/shop/geeksasang/domain/chat/Chat.java
@@ -1,6 +1,7 @@
 package shop.geeksasang.domain.chat;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,6 +17,7 @@ import java.util.List;
 @Document //@Document는객체를 몽고DB에 영속화시킴 = SpringDataJpa의 @Entity와 같은 역할
 @ToString
 @Getter
+@NoArgsConstructor
 public class Chat implements Serializable {
 
     @Id
