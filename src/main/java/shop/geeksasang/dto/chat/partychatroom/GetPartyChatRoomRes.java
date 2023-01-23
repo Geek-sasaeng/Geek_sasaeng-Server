f1322604cc47b30ba9292f8f7adfcb81d5361246
xhaktmchl <xhaktmchl@naver.com>
2023-01-23 14:54:14 +0900

    #237 feat: 채팅방 목록 조회API-채팅방 최초 입장 시간res에 enterTime필드 추가

--- src/main/java/shop/geeksasang/dto/chat/partychatroom/GetPartyChatRoomRes.java
+++ src/main/java/shop/geeksasang/dto/chat/partychatroom/GetPartyChatRoomRes.java
@@ -1,9 +1,13 @@
 package shop.geeksasang.dto.chat.partychatroom;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import shop.geeksasang.domain.chat.PartyChatRoom;
+import shop.geeksasang.domain.chat.PartyChatRoomMember;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -15,12 +19,17 @@ public class GetPartyChatRoomRes {
     @ApiModelProperty(example = "데빈의 채팅방", value = "채팅방 정보 리스트")
     private String roomTitle;
 
-    public GetPartyChatRoomRes(String roomId, String title) {
+    @ApiModelProperty(example = "2023-01-03 11:00:12", value = "채팅방 최초 입장시간")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime enterTime;
+
+    public GetPartyChatRoomRes(String roomId, String title, LocalDateTime enterTime) {
         this.roomId = roomId;
         this.roomTitle = title;
+        this.enterTime = enterTime;
     }
 
-    public static GetPartyChatRoomRes of(PartyChatRoom partyChatRoom){
-        return new GetPartyChatRoomRes(partyChatRoom.getId(), partyChatRoom.getTitle());
+    public static GetPartyChatRoomRes of(PartyChatRoom partyChatRoom, PartyChatRoomMember partyChatRoomMember){
+        return new GetPartyChatRoomRes(partyChatRoom.getId(), partyChatRoom.getTitle(), partyChatRoomMember.getEnterTime());
     }
 }
