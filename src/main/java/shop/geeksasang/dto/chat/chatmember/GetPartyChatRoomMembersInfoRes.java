ce6ad687a2c57074fbcf03070bffddb1d5524896
happysubin <asb0711@gmail.com>
2023-01-30 17:18:30 +0900

    Feat: 스웨거 애노테이션 추가

--- src/main/java/shop/geeksasang/dto/chat/chatmember/GetPartyChatRoomMembersInfoRes.java
+++ src/main/java/shop/geeksasang/dto/chat/chatmember/GetPartyChatRoomMembersInfoRes.java
@@ -1,5 +1,6 @@
 package shop.geeksasang.dto.chat.chatmember;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import shop.geeksasang.domain.chat.PartyChatRoomMember;
@@ -10,12 +11,18 @@ import shop.geeksasang.domain.member.Member;
 @NoArgsConstructor
 public class GetPartyChatRoomMembersInfoRes {
 
+    @ApiModelProperty(example = "63d77adea2639d2b72c7e743", value = "멤버 uuid")
     private String memberUuid;
 
+    @ApiModelProperty(example = "22", value = "맴버 id")
     private Integer memberId;
 
+    @ApiModelProperty(example = "debin", value = "멤버 닉네임")
     private String userName;
 
+    @ApiModelProperty(
+            example = "https://geeksasaeng-s3.s3.ap-northeast-2.amazonaws.com/%ED%94%84%EB%A1%9C%ED%95%84+%EC%9D%B4%EB%AF%B8%EC%A7%80/baseProfileImg.png",
+            value = "멤버 프로필 url")
     private String userProfileImgUrl;
 
     public GetPartyChatRoomMembersInfoRes(String memberUuid, Integer memberId, String userName, String userProfileImgUrl) {
