a26def13e1ba8afe00bff20b24b6bfeaf26ad89e
happysubin <asb0711@gmail.com>
2023-02-17 15:23:14 +0900

    Fix: 중첩 클래스 변경

---
+++ src/main/java/shop/geeksasang/controller/applelogin/model/Name.java
@@ -0,0 +1,20 @@
+package shop.geeksasang.controller.applelogin.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Name {
+
+    private String lastName;
+    private String firstName;
+
+
+    public Name(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+}
